8e3d197eda6deb695c8f750159ce1b2bdb8c3ee2
MohammedRasif <mohammed660012@gmail.com>
2026-05-21 16:03:29 +0600

    moduler desginer

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,12 @@
-import express, { type Application, type Request, type Response } from 'express';
+import app from "./app";
+import { initDB } from "./db/index"
+import config from "./config";
 
-import{Pool} from 'pg'
-import { initDB } from './db/index';
-import config from './config/index';
-
-initDB();
-
-const app: Application = express()
-const port = config.port;
-
-app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello World!')
-    res.status(200).json({
-        "message" : "Express Server",
-        "author" : "Next Level"
-    })
+const main = () => {
+ initDB();
+  app.listen(config.port, () => {
+    console.log(`Server listening on port ${config.port}`)
 })
+}
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+main()
